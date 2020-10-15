4f935d674aedc24463ecdde1c7ebd7a8cfaac02f
FOZLAUR <fozla@oocl.com>
2020-10-15 14:03:12 +0800

    [fizz-buzz-whizz][LaurenceFoz] test: Added test case for normal number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,14 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_order_number_when_count_off_given_normal_number() throws Exception{
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 1;
+        String actual = fizzBuzz.countOff(input);
+        assertEquals("1", actual);
+    }
 }
