5bcf758e0cc758c0f4411583b20c44c0cf5abc77
FOZLAUR <fozla@oocl.com>
2020-10-15 14:11:48 +0800

    [fizz-buzz-whizz][LaurenceFoz] feature: Added logic for countOff given multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int input){
+        if(input % 3 == 0){
+            return "Fizz";
+        }
         return String.valueOf(input);
     }
 }
