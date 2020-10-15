1070b9fcf3b494c1cff3aa3564c6bd0467228555
FOZLAUR <fozla@oocl.com>
2020-10-15 14:34:03 +0800

    [fizz-buzz-whizz][LaurenceFoz] refactor: Created constant for string "FizzBuzz"

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,13 +4,14 @@ public class FizzBuzz {
     final private static String FIZZ = "Fizz";
     final private static String BUZZ = "Buzz";
     final private static String WHIZZ = "Whizz";
+    final private static String FIZZBUZZ = "FizzBuzz";
     final private static int MOD3 = 3;
     final private static int MOD5 = 5;
     final private static int MOD7 = 7;
 
     public String countOff(int input){
         if(input % MOD3 == 0 && input % MOD5 == 0){
-            return "FizzBuzz";
+            return FIZZBUZZ;
         }
         if(input % MOD3 == 0){
             return FIZZ;
