2af9dd9cdb0a6db2b7b9e15c6b8f0d21b864d701
FOZLAUR <fozla@oocl.com>
2020-10-15 14:46:26 +0800

    [fizz-buzz-whizz][LaurenceFoz] test: Added test case for multiple of 5 and 7

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -44,12 +44,19 @@ public class FizzBuzzTest {
         assertEquals("FizzBuzz", actual);
     }
 
-
     @Test
     public void should_fizz_whizz_when_count_off_given_multiple_of_three_and_seven() throws Exception {
         FizzBuzz fizzBuzz = new FizzBuzz();
         int input = 21;
         String actual = fizzBuzz.countOff(input);
         assertEquals("FizzWhizz", actual);
+    }
+
+    @Test
+    public void should_buzz_whizz_when_count_off_given_multiple_of_five_and_seven() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 21;
+        String actual = fizzBuzz.countOff(input);
+        assertEquals("BuzzWhizz", actual);
     }
 }
