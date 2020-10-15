582d628a247366ee33b43347f6128c548220d081
FOZLAUR <fozla@oocl.com>
2020-10-15 14:08:10 +0800

    [fizz-buzz-whizz][LaurenceFoz] test: Added test case for multiple of 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,10 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void should_order_number_when_count_off_given_normal_number() throws Exception{
+    public void should_order_number_when_count_off_given_normal_number() throws Exception {
         FizzBuzz fizzBuzz = new FizzBuzz();
         int input = 1;
         String actual = fizzBuzz.countOff(input);
         assertEquals("1", actual);
+    }
+
+    @Test
+    public void should_fizz_when_count_off_given_multiple_of_three() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 3;
+        String actual = fizzBuzz.countOff(input);
+        assertEquals("Fizz", actual);
     }
 }
